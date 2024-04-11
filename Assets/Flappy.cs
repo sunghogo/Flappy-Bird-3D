74f5ac1cc118fa6ac9833db330993878016274cf
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-04-11 00:35:58 -0400

    Partially added rotation

--- Assets/Flappy.cs
+++ Assets/Flappy.cs
@@ -18,12 +18,30 @@ public class Flappy : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.Space)) {
+            RotateUp();
             Fly();
         }
     }
 
+    private void FixedUpdate() {
+        RotateDown();
+    }
+
+    // Can't properly check negative/full positive ranges 
+    private void RotateDown() {
+        if ((0 <= transform.eulerAngles.x && transform.eulerAngles.x <= 30) || (-30 <= transform.eulerAngles.x && transform.eulerAngles.x <= 0))
+            transform.eulerAngles += new Vector3(1, 0, 0);
+        else if (30 < transform.eulerAngles.x && transform.eulerAngles.x < 33)
+            transform.eulerAngles = new Vector3(30, 0, 0);
+    }
+
+    private void RotateUp() {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+
     private void Fly() {
         _rigidbody.AddForce(Vector3.up * Time.deltaTime * _flySpeed);
+
     }
 
 }
